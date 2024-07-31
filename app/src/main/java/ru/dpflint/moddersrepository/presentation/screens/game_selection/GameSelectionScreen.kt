556fc95ef1fc8ef21adc480f2be1458cc8f0ab82
DopamineFlint <dopamine.flint@gmail.com>
2024-07-31 19:04:05 +0300

    Adding TopBar to GameSelectionScreen

--- app/src/main/java/ru/dpflint/moddersrepository/presentation/screens/game_selection/GameSelectionScreen.kt
+++ app/src/main/java/ru/dpflint/moddersrepository/presentation/screens/game_selection/GameSelectionScreen.kt
@@ -2,9 +2,17 @@ package ru.dpflint.moddersrepository.presentation.screens.game_selection
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 import ru.dpflint.moddersrepository.domain.model.GameModel
 import ru.dpflint.moddersrepository.presentation.components.CustomTopBar
@@ -28,6 +37,15 @@ import ru.dpflint.moddersrepository.presentation.viewmodel.MainViewModel
 fun GameSelectionScreen(
     viewModel: MainViewModel = koinViewModel()
 ) {
+
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.handleIntent(
+            intent = ModsIntent.LoadGamesFromNexus
+        )
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -42,31 +60,21 @@ fun GameSelectionScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Text", color = Color.White)
+            when {
+                state.isLoading -> {
+                    LoadingBar()
+                }
+                state.error != null -> {
+                    ErrorMessage(state.error)
+                }
+                else -> {
+                    GameSelectionList(
+                        gamesList = state.games
+                    )
+                }
+            }
         }
     }
-
-    //val state by viewModel.state.collectAsState()
-
-//    LaunchedEffect(key1 = true) {
-//        viewModel.handleIntent(
-//            intent = ModsIntent.LoadGamesFromNexus
-//        )
-//    }
-
-//    when {
-//        state.isLoading -> {
-//            LoadingBar()
-//        }
-//        state.error != null -> {
-//            ErrorMessage(state.error)
-//        }
-//        else -> {
-//            GameSelectionList(
-//                gamesList = state.games
-//            )
-//        }
-//    }
 }
 
 @Composable
@@ -74,8 +82,28 @@ private fun GameSelectionList(
     gamesList: List<GameModel>
 ) {
     LazyColumn(
-
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(10.dp)
     ) {
-
+        items(gamesList.size) { i ->
+            Row(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillParentMaxWidth()
+            ) {
+                Text(
+                    text = gamesList[i].name,
+                    color = Color.Black
+                )
+                Icon(
+                    imageVector = Icons.Default.Add, //TODO
+                    contentDescription = "Icon"
+                )
+                Checkbox(
+                    checked = false,
+                    onCheckedChange = {},
+                )
+            }
+        }
     }
 }
